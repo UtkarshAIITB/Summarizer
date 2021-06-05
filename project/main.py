e07e5a739434653fdecc4ef4b88cc96ab9082e30
UtkarshAIITB <200020154@iitb.ac.in>
2021-06-06 01:20:19 +0530

    Summarizer

--- project/main.py
+++ project/main.py
@@ -20,15 +20,43 @@ def home():
 def pdf1():
     return render_template('pdf.htm')
 
-@app.route('/uploader', methods = ['GET', 'POST'])
+@app.route('/upload', methods = ['GET', 'POST'])
 def upload_file():
-   if request.method == 'POST':
-       f = request.files['file']
-       global file_name
-       f.save(secure_filename(f.filename))
-       file_name = f.filename
-       return('file uploaded successfully')
+    if request.method == 'POST':
+        f = request.files['file']
+        global file_name
+        f.save(secure_filename(f.filename))
+        file_name = f.filename
+        #return('file uploaded successfully')
+        
+   
+        #def pdf():
+        fileobj = open(file_name, 'rb')
+        pdfreader = PyPDF2.PdfFileReader(fileobj)
+        page = pdfreader.numPages                                      #stores total no. of pages
+        text = ""
+            
+        for x in range(page):
+            pageObj = pdfreader.getPage(x)
+            parts = pageObj.extractText()                              #extracts the text from the pdf file of a particular page in parts
+            #parts = parts.replace(' ', '-')
+            text +=parts                                               #stored the complete data of pdf in the text 
 
+            #text = re.sub("-"," ", text)
+        text = text.replace("\n", " ")
+
+        with open('summary.txt', 'w', encoding = 'utf-8') as s:        #storing the extracted data from pdf into text file 
+            s.write(text)
+
+        final = generate_summary("summary.txt", 2) 
+            
+
+        fileobj.close()
+        #return render_template('pdf.htm' , page = page)
+
+    return render_template('pdf.htm', final = final)
+
+'''
 @app.route('/pdfpage')
 def pdf():
     fileobj = open(file_name, 'rb')
@@ -48,10 +76,13 @@ def pdf():
     with open('summary.txt', 'w', encoding = 'utf-8') as s:                #storing the extracted data from pdf into text file 
         s.write(text)
 
-    return( generate_summary("summary.txt", 2) )
+    final =  generate_summary("summary.txt", 2) 
+    return(final)
     
     fileobj.close()
     #return render_template('pdf.htm' , page = page)
+'''
+
 
 
 if __name__ == "__main__":
